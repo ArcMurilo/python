bf6fbbd0021c837ec20d56e05ea02e568d42570d
ArcMurilo <murilo.moraes91@gmail.com>
2014-04-28 14:15:51 -0300

    ex. Cesar Code

---
+++ Cesar Code/buildCoder.py
@@ -0,0 +1,22 @@
+import string
+def buildCoder(shift):
+    """
+    Returns a dict that can apply a Caesar cipher to a letter.
+    The cipher is defined by the shift value. Ignores non-letter characters
+    like punctuation, numbers, and spaces.
+
+    shift: 0 <= int < 26
+    returns: dict
+    """
+
+    assert (type(shift) == int and 0 <= shift and shift <= 26)
+
+    coder = {}
+
+    coder.
+
+    print type(string.ascii_lowercase)
+
+
+
+    ### TODO 

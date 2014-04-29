ecb952f9ee9d621f3dd32b0c0e6243eff02eb56d
ArcMurilo <murilo.moraes91@gmail.com>
2014-04-29 08:20:21 -0300

    Update buildCoder
    Corrigir erros na lógica.

--- Cesar Code/buildCoder.py
+++ Cesar Code/buildCoder.py
@@ -8,14 +8,14 @@ def buildCoder(shift):
     shift: 0 <= int < 26
     returns: dict
     """
-
     assert (type(shift) == int and 0 <= shift and shift <= 26)
     coder = {}
     element = 1
-    for i in range((0 - shift, 26 - shift):
+    for i in range(26 - shift):
+        coder[element] = string.ascii_uppercase[i + shift]
+        element += 1
+    for i in range(shift):
         coder[element] = string.ascii_uppercase[i]
+        element += 1
         
     return coder
-
-
-    ### TODO 
